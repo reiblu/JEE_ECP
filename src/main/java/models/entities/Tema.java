e4d0d719972925779513097ee1797d4817a667fb
Leire <Leire@Rei>
2015-03-04 19:42:14 +0100

    Añadida clase enumerado estudios

--- src/main/java/models/entities/Tema.java
+++ src/main/java/models/entities/Tema.java
@@ -1,5 +1,16 @@
 package models.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Tema {
 
+	public static final String PREGUNTA = "pregunta";
+	
+	@Id
+    @GeneratedValue
+    private Integer id;
+	
 }
---
+++ src/main/java/models/utils/Estudios.java
@@ -0,0 +1,5 @@
+package models.utils;
+
+public enum Estudios {
+	ESO, BACHILLERATO, GRADUADO;
+}
