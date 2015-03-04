3c32b84822ff65c4addd96703a4688bbc8e526c8
Leire <Leire@Rei>
2015-03-04 19:51:53 +0100

    Entidad tema implementada

--- src/main/java/models/entities/Tema.java
+++ src/main/java/models/entities/Tema.java
@@ -1,16 +1,55 @@
 package models.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+
+import models.utils.Estudios;
+
 
 @Entity
 public class Tema {
 
 	public static final String PREGUNTA = "pregunta";
 	
+	public static final String NOMBRE = "nombre";
+	
 	@Id
     @GeneratedValue
     private Integer id;
+	
+	@Enumerated(EnumType.STRING)
+	private Estudios estudios;
+	
+	public Tema(Estudios estudios){
+		this.estudios = estudios;
+	}
+	
+	public Tema(){
+		
+	}
+	
+	public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    public Estudios getEstudios(){
+    	return estudios;
+    }
+    
+    public void setEstudios(Estudios estudio){
+    	this.estudios = estudio;
+    }
+    
+    @Override
+    public String toString() {
+        return "Tema [id=" + id + ", estudios=" + estudios + "]";
+    }
 	
 }
