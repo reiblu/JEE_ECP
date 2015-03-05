28fc8bee1a4b5a8e2953b9a9362686f25bfa55b4
Leire <leirehe@gmail.com>
2015-03-05 13:46:46 +0100

    Creado test dao tema

---
+++ src/test/java/models/daos/jpa/test/TemaDaoJpaTest.java
@@ -0,0 +1,39 @@
+package models.daos.jpa.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TemaDaoJpaTest {
+
+    @Test
+    public void testCreate() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testRead() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testUpdate() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testDeleteById() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testFindAll() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testDeleteByID() {
+        fail("Not yet implemented");
+    }
+
+}
