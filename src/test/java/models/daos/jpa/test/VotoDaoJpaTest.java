44d6a42110a3e9669a64e53b83c73927f3c8f9d5
Leire <leirehe@gmail.com>
2015-03-10 10:40:05 +0100

    Test voto hecho

--- src/test/java/models/daos/jpa/test/VotoDaoJpaTest.java
+++ src/test/java/models/daos/jpa/test/VotoDaoJpaTest.java
@@ -1,64 +1,73 @@
 package models.daos.jpa.test;
 
 import static org.junit.Assert.*;
-
-import javax.persistence.EntityManager;
-import javax.persistence.Persistence;
-import javax.persistence.Query;
-
+import models.daos.VotoDao;
+import models.daos.jpa.DaoJpaFactory;
 import models.entities.Tema;
 import models.entities.Voto;
 import models.utils.Estudios;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class VotoDaoJpaTest {
 
-	 EntityManager em = Persistence.createEntityManagerFactory("JEE_ECP").createEntityManager();
-	 
-	 Tema t1 = new Tema("¿Te ha gustado?","Juegos");
-	 Voto v1 = new Voto(Estudios.ESO, "192.0.0.0", 3, t1);
-	
-	@Test
-	public void testCreate() {
-		em.getTransaction().begin();
-        em.persist(v1);
-        em.persist(new Voto(Estudios.BACHILLERATO, "0.0.0.0", 5,t1));
-        em.getTransaction().commit();
-	}
+    private Tema t1;
 
-	@Test
-	public void testRead() {
-		Voto v3 = em.find(Voto.class, "192.0.0.0");
-    	assertEquals(v3,v1);
-	}
+    private Voto v1;
 
-	@Test
-	public void testUpdate() {
-		em.getTransaction().begin();
-        Voto v4 = em.merge(new Voto(Estudios.GRADUADO, "0.0.0.0", 5,t1));
-        em.getTransaction().commit();
-        assertEquals("Voto [ipUsuario=0.0.0.0 valoracion=5 id_tema=" + t1.getId() + "estudios=" + Estudios.GRADUADO + "]",v4);
-        
-        em.getTransaction().begin();
-        v1.setValoracion(7);
-        em.getTransaction().commit();
-	}
+    private VotoDao dao = DaoJpaFactory.getFactory().getVotoDao();
 
-	@Test
-	public void testDeleteByID() {
-		Query query = em.createQuery("SELECT id FROM voto WHERE ipUsuario='0.0.0.0' AND valoracion='7'");
-        int id = query.getFirstResult();
-        Voto v2 = em.find(Voto.class, id);
-        em.getTransaction().begin();
-        em.remove(v2);
-        em.getTransaction().commit();
-	}
+    @BeforeClass
+    public void init() {
+        DaoJpaFactory.setFactory(new DaoJpaFactory());
+        DaoJpaFactory.prepareFactoryWithDropAndCreateTables();
+    }
 
-	@Test
-	public void testFindAll() {
-		Query query = em.createQuery("SELECT e FROM voto e");
-   	 	query.getResultList();
-	}
+    @Before
+    public void init1() {
+        t1 = new Tema("Â¿Te ha gustado?", "Juegos");
+        v1 = new Voto(Estudios.ESO, "192.0.0.0", 3, t1);
+        dao.create(v1);
+    }
+
+    @Test
+    public void testCreate() {
+        Voto v2 = new Voto(Estudios.GRADUADO, "192.0.0.0", 5, t1);
+        dao.create(v2);
+        assertEquals(v2, dao.read(v2.getId()));
+    }
+
+    @Test
+    public void testRead() {
+        assertEquals(this.v1, dao.read(v1.getId()));
+    }
+
+    @Test
+    public void testUpdate() {
+        v1.setValoracion(6);
+        dao.update(v1);
+        assertEquals(this.v1, dao.read(v1.getId()));
+    }
+
+    @Test
+    public void testDeleteByID() {
+        Voto v4 = new Voto(Estudios.BACHILLERATO, "192.0.0.0", 9, t1);
+        dao.create(v4);
+        dao.deleteByID(v4.getId());
+        assertNull(dao.read(v4.getId()));
+    }
+
+    @Test
+    public void testFindAll() {
+        assertEquals(2, dao.findAll().size());
+    }
+    
+    @After
+    public void after() {
+        DaoJpaFactory.prepareFactoryWithDropAndCreateTables();
+    }
 
 }
