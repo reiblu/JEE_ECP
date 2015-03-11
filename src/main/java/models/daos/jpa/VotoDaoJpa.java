7ace8335fe54363677a1721ac69970fc83d71477
Leire <Leire@Rei>
2015-03-11 19:21:00 +0100

    Implementacion del borrar todos los votos

--- src/main/java/models/daos/jpa/VotoDaoJpa.java
+++ src/main/java/models/daos/jpa/VotoDaoJpa.java
@@ -1,12 +1,31 @@
 package models.daos.jpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaDelete;
+import javax.persistence.criteria.Root;
+
 import models.daos.VotoDao;
+import models.entities.Tema;
 import models.entities.Voto;
 
 public class VotoDaoJpa extends GenericDaoJpa<Voto, Integer> implements VotoDao {
 
     public VotoDaoJpa() {
         super(Voto.class);
+    }
+    
+    EntityManager em = DaoJpaFactory.getEntityManagerFactory().createEntityManager();
+    CriteriaBuilder criteria = em.getCriteriaBuilder();	
+    
+    public void BorrarVotosdeTema (Tema tema){
+    	
+    	em.getTransaction().begin();
+    	CriteriaDelete<Voto> borrar = criteria.createCriteriaDelete(Voto.class);
+	    Root<Voto> e = borrar.from(Voto.class);
+	    borrar.where(criteria.equal(e.get("tema").get("id"), tema.getId()));
+	    em.getTransaction().commit();
+    	
     }
 
 }
